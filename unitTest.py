e1bac6a353f01db65bb64c1f73e7df9e672b3fd9
James <jamessoh11@gmail.com>
2020-10-01 20:50:16 +0800

    Added unitTest for storage

--- unitTest.py
+++ unitTest.py
@@ -1,10 +1,24 @@
-from code import quiz
+from code import quiz, storage, player
+import unittest
 
-s = "is the sun hot?"
-ans = 1
-Tfq = quiz.Tfq(s, 1)
+class TestStorageMethods(unittest.TestCase):
 
-s = "what school are we in"
-choices = ['nus', 'ntu', 'smu', 'sutd']
-ans = 1
-Mcq = quiz.Mcq(s, ans, choices)
+    def test_getQuestions(self):
+        print("Testing getQuestions()")
+        s = storage.Storage()
+        # getQuestions currently reads from hardcoded location questions.txt.
+
+        actual_results = s.getQuestions()
+        '''
+        Sample data:
+        "Here is a sample mcq qn, with some puncuation!!" | mcq | "this is choice 1, nice" , "ntu" , "smu" , "sutd" | 1
+        "sun is blue" | "tf" | 0
+        '''
+        expected = []
+        expected.append(quiz.Mcq("Here is a sample mcq qn, with some puncuation!!", 1, ["this is choice 1, nice" , "ntu" , "smu" , "sutd"]))
+        expected.append(quiz.Tfq("sun is blue", 0))
+
+        self.assertEqual(actual_results, expected)
+
+if __name__ == '__main__':
+    unittest.main()
