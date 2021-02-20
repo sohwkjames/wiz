0f3026eaafb6a6dbcb82633f57baf9c7bb403143
James <jamessoh11@gmail.com>
2021-02-20 21:50:31 +0800

    Added player bar

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,6 +3,7 @@ import './App.css';
 import React, { Component } from 'react';
 
 // ModifiableGameArea
+//   AddPlayerbar
 //   PlayerList
 //      PlayerRow
 //   QuestionArea
@@ -15,7 +16,17 @@ class PlayerRow extends React.Component{
   render(){
     var name = this.props.name
     var score = this.props.score
-    return (<li>Player: {name}, Score: {score}</li>  )
+    var player_row;
+    if (this.props.current_player){
+      return (
+        <b><li>Player: {name}, Score: {score}</li></b>
+      )  
+    }
+    else{
+      return (
+        <li>Player: {name}, Score: {score}</li>
+      )
+    }
   }
 }
 
@@ -26,10 +37,10 @@ class PlayerList extends React.Component{
       var listitems = []
       players.forEach((player) => {
         if (player.id === current_player_id){
-          //listitems.push(<PlayerRow key={player.id} name={player.name} score={player.score}/>)
+          listitems.push(<PlayerRow key={player.id} name={player.name} score={player.score} current_player={true}/>)
         }
         else{
-          listitems.push(<PlayerRow key={player.id} name={player.name} score={player.score}/>)
+          listitems.push(<PlayerRow key={player.id} name={player.name} score={player.score} current_player={false}/>)
         }
       })
       return (
@@ -45,7 +56,6 @@ class QuestionArea extends React.Component {
 
   getQuestion(qn_number){
     // Takes a question number, calls an api, returns a questin object.
-
     var qn_objects = {1: {"question_id": 1, "text": "what is 2+2?","choice1": "3","choice2": "4", "correct_answer":"4"},
      2: {"question_id": 2, "text": "where is singapore?",
      "choice1": "asia",
@@ -54,6 +64,8 @@ class QuestionArea extends React.Component {
     return qn_objects[qn_number]
   }
 
+
+
   render() {
     const current_question = this.props.current_question
     var qn_object = this.getQuestion(current_question);
@@ -61,8 +73,21 @@ class QuestionArea extends React.Component {
     return (
       <div>
         {qn_object["text"]}
-        <button>{qn_object["choice1"]}</button>
-        <button>{qn_object["choice2"]}</button>
+        <button onClick={this.props.handleAddScore}>{qn_object["choice1"] }</button>
+        <button onClick={this.props.handleAddScore}>{qn_object["choice2"]}</button>
+      </div>
+    )
+  }
+}
+
+class AddPlayerBar extends React.Component{
+  // renders a text input, a addPlayer button, a startGame button.
+  render() {
+    return (
+      <div>
+        <input type="text" id="playerName" onChange={this.props.handleOnChange}></input>
+        <button id="addPlayer" onClick={this.props.handleOnClick}>Add a Player</button>
+        <div><button id="startGame" onClick={this.props.onStartGame}>Start Game</button></div>
       </div>
     )
   }
@@ -80,23 +105,24 @@ class ModifiableGameArea extends React.Component{
       questions: [],
       player_to_add: "",
       game_started: false,
-      current_player: "none",
+      current_player_id: 0,
       current_question: 1,
     }
-    this.handleClick = this.handleClick.bind(this);
-    this.handleChange = this.handleChange.bind(this);
+    this.handleAddPlayerClick = this.handleAddPlayerClick.bind(this);
+    this.handlePlayerNameChange = this.handlePlayerNameChange.bind(this);
     this.handleStartGame = this.handleStartGame.bind(this);
+    this.addPointToPlayer = this.addPointToPlayer.bind(this);
+
   }
 
-  handleClick(){
+  handleAddPlayerClick(){
     var newArray = this.state.players.slice()
-    var newDict =  {"id": this.state.players.length, "name": this.state.player_to_add, "score": 0}
-    newArray.push(newDict)
+    var newPlayer =  {"id": this.state.players.length, "name": this.state.player_to_add, "score": 0}
+    newArray.push(newPlayer)
     this.setState({ players: newArray })
-    //alert (this.state.players)
   }
   
-  handleChange(e){
+  handlePlayerNameChange(e){
     var player_name = e.target.value
     this.setState( {player_to_add: player_name} )
   }
@@ -106,25 +132,36 @@ class ModifiableGameArea extends React.Component{
       alert(this.state.game_started + "state of game")
     })
   }
+
+  addPointToPlayer(){
+    
+    var newArray = this.state.players.slice()
+    console.log("current player id", this.props.current_player_id)
+    console.log(newArray[this.props.current_player_id] )
+    console.log(newArray)
+    newArray[this.props.current_player_id] ++
+    this.setState({players: newArray})
+  }
+
   // Add question. Play 
   render() {
     const game_started = this.state.game_started;
     let question_area;
+    let add_player_bar;
     if (game_started){
-      question_area = <QuestionArea current_question={this.state.current_question}/>
-      
+      question_area = <QuestionArea current_question={this.state.current_question}
+                        handleAddScore={this.addPointToPlayer} 
+                        current_player_id={this.state.current_player_id}/>
     }
     else{
-      question_area = null
+      question_area = null;
+      add_player_bar = <AddPlayerBar handleOnChange={this.handlePlayerNameChange} handleOnClick={this.handleAddPlayerClick} onStartGame={this.handleStartGame}/>
     }
     return (
       <div>
-        <input type="text" id="playerName" onChange={this.handleChange}></input>
-        <button id="addPlayer" onClick={this.handleClick}>Add a Player</button>
-        <div><button id="startGame" onClick={this.handleStartGame}>Start Game</button></div>
-        <PlayerList players={this.state.players} current_player_id="1"/>
+        {add_player_bar}
+        <PlayerList players={this.state.players} current_player_id={this.state.current_player_id}/>
         {question_area}
-
       </div>
     )
   }
@@ -132,14 +169,10 @@ class ModifiableGameArea extends React.Component{
 
 function App() {
   return (
-    
     <div className="App">
       <header className="App-header">
         <div className="playerArea"> <ModifiableGameArea/> </div>
       </header>
-
-        
-      
     </div>
   );
 }
